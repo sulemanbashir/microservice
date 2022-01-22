b50b52129a596fbb0d696cdeafd268b519759602
Suleman Bashir <31032389+sulemanbashir@users.noreply.github.com>
2022-01-23 02:55:45 +0500

    Config Server and Service Registry

---
+++ client-service/src/test/java/com/ecommerce/service/client/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.service.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ summary-service/src/main/java/com/ecommerce/service/summary/SummaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.service.summary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummaryServiceApplication.class, args);
+	}
+
+}
