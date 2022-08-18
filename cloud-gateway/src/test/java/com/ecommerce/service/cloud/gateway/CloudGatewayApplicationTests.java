e770d14fa45f3d41d356ae6e4561375679537207
Suleman Bashir <f158053@nu.edu.pk>
2022-08-18 11:21:16 +0500

    API Gateway

---
+++ cloud-gateway/src/test/java/com/ecommerce/service/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.service.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
